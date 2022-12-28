203cb33ce410a9a6b1794c31f2e8441e8052c9bc
netanmangal <imnetanmangal@gmail.com>
2022-12-28 19:46:36 +0530

    Add: rocket init

--- v1/src/main.rs
+++ v1/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{self, get, launch, routes};
+
+#[get("/")]
+pub fn index() -> String {
+    return format!("Welcome to homepage!");
+}
+
+#[launch]
+fn launch() -> _ {
+    rocket::build().mount("/", routes![index])
 }
