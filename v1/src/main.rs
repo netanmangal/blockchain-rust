c1e2e34e58871489a2404d842824a7c1b8ef11e7
netanmangal <imnetanmangal@gmail.com>
2022-12-28 22:21:32 +0530

    Add: db connection mongodb

--- v1/src/main.rs
+++ v1/src/main.rs
@@ -1,10 +1,20 @@
 use rocket::{self, launch};
+use dotenv::dotenv;
 
+mod db;
 mod models;
 mod controllers;
 
+use db::DB;
+
 #[launch]
-fn launch() -> _ {
+async fn launch() -> _ {
+    dotenv().ok();
+
+    let db: DB = DB::connect_db().await.unwrap();
+    let database_name: String = dotenv::var("DATABASE_NAME").expect("Expected env variable: DATABASE_NAME");
+
     rocket::build()
         .mount("/", controllers::all())
+        .manage(db.client.database( &database_name ))
 }
