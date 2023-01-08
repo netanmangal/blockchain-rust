799f7ddb9ac5af1ba4619cf1fb309e6c338b2e73
netanmangal <imnetanmangal@gmail.com>
2023-01-09 00:05:49 +0530

    Add: port and db name based on arg to command line

--- v1/src/main.rs
+++ v1/src/main.rs
@@ -1,5 +1,5 @@
 use dotenv::dotenv;
-use rocket::{self, launch};
+use rocket::{self, launch, Config};
 
 mod controllers;
 mod db;
@@ -11,13 +11,24 @@ use db::DB;
 
 #[launch]
 async fn launch() -> _ {
+    // start the server using - cargo run localhost:8001
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("start the server using - cargo run localhost:8001");
+    }
+
     dotenv().ok();
+
+    let port: &str = args[1].split(':').collect::<Vec<&str>>()[1];
 
     let db: DB = DB::connect_db().await.unwrap();
     let database_name: String =
-        dotenv::var("DATABASE_NAME").expect("Expected env variable: DATABASE_NAME");
+        dotenv::var("DATABASE_NAME").expect("Expected env variable: DATABASE_NAME") + "-" + port;
 
-    rocket::build()
+    let rocket_figment = Config::figment().merge(("port", port.parse::<u16>().unwrap()));
+
+    rocket::custom(rocket_figment)
         .mount("/", controllers::all())
         .manage(db.client.database(&database_name))
 }
